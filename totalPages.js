f08f3f0116d4599a2ef348d9ba1c93832b8e2db8
RewEPIC <78597357+RewEPIC@users.noreply.github.com>
2022-09-08 21:32:36 +0700

    Update totalPages.js

--- totalPages.js
+++ totalPages.js
@@ -1,8 +1,6 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-    return !arrayItems ? undefined : 
-           (!rowsPerPage || rowsPerPage === 0 ? 
-           1 : Math.ceil(arrayItems.length / rowsPerPage))
+    return !arrayItems ? undefined : (!rowsPerPage ? 1 : Math.ceil(arrayItems.length / rowsPerPage))
 }
 module.exports = totalPages
